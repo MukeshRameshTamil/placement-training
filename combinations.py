002ca910c7877d5c1ef281c77fbd6933cd943802
MukeshRameshTamil <mukesh200424@gmail.com>
2024-07-23 20:10:40 +0530

    Create combinations.py

---
+++ combinations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combine(self, n: int, k: int) -> list[list[int]]:
+        T = TypeVar('T')
+        def combinations(pool: Iterable[T], r: int = None) -> Iterator[Iterator[T]]:
+            pool = tuple(pool)
+            r = len(pool) if r is None else r
+
+            yield from ((
+                (x,) + c
+                for i, x in enumerate(pool)
+                for c in combinations(pool[i + 1:], r - 1)
+            ) if pool else ()) if r else ((),)
+        
+        return list(combinations(range(1, n + 1), k))
